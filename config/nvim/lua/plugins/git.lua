3821e2341f430b1404073d350cd475931494fe25
Fredric Hegland <114155995+frheg@users.noreply.github.com>
2026-06-10 21:41:37 +0200

    enable automatic git blame and signs

--- config/nvim/lua/plugins/git.lua
+++ config/nvim/lua/plugins/git.lua
@@ -4,11 +4,55 @@ return {
     event = { "BufReadPre", "BufNewFile" },
 
     opts = {
-      current_line_blame = false,
+      signs = {
+        add          = { text = "│" },
+        change       = { text = "│" },
+        delete       = { text = "_" },
+        topdelete    = { text = "‾" },
+        changedelete = { text = "~" },
+      },
+
+      signcolumn = true,
+      numhl      = false,
+      linehl     = false,
+
+      current_line_blame = true,
 
       current_line_blame_opts = {
-        delay = 300,
+        delay = 200,
+        virt_text = true,
         virt_text_pos = "eol",
+      },
+
+      preview_config = {
+        border = "rounded",
+      },
+
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        local map = function(mode, lhs, rhs, desc)
+          vim.keymap.set(mode, lhs, rhs, {
+            buffer = bufnr,
+            desc = desc,
+          })
+        end
+
+        map("n", "]h", gs.next_hunk, "Next hunk")
+        map("n", "[h", gs.prev_hunk, "Previous hunk")
+
+        map("n", "<leader>gb", gs.blame_line, "Blame line")
+        map("n", "<leader>gB", function()
+          gs.blame_line({ full = true })
+        end, "Full blame")
+
+        map("n", "<leader>gp", gs.preview_hunk, "Preview hunk")
+        map("n", "<leader>gr", gs.reset_hunk, "Reset hunk")
+        map("n", "<leader>gs", gs.stage_hunk, "Stage hunk")
+
+        map("n", "<leader>gtb", gs.toggle_current_line_blame,
+          "Toggle blame")
+        end,
       },
 
       on_attach = function(bufnr)
