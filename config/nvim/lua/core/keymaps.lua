6adfa9f72c6ad68bc731c7d5eb035be37d1f5217
Fredric Hegland <fredric.hegland@gmail.com>
2026-06-11 23:23:47 +0200

    add buffer navigation keymaps

--- config/nvim/lua/core/keymaps.lua
+++ config/nvim/lua/core/keymaps.lua
@@ -12,3 +12,10 @@ map("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
 
 map("n", "<leader>sm", "<cmd>messages<CR>", { desc = "Show messages" })
 map("n", "<leader>gg", "<cmd>LazyGitPopup<CR>", { desc = "Open lazygit popup" })
+
+-- Buffers
+map("n", "<S-l>", "<cmd>bnext<CR>", { desc = "Next buffer" })
+map("n", "<S-h>", "<cmd>bprevious<CR>", { desc = "Previous buffer" })
+map("n", "<leader>bd", "<cmd>bdelete<CR>", { desc = "Delete buffer" })
+map("n", "<leader>bn", "<cmd>enew<CR>", { desc = "New buffer" })
+map("n", "<leader>bb", "<cmd>Telescope buffers<CR>", { desc = "List buffers" })
